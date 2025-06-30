db2049c559e5d46682c1cfb16e0f286897ef1f43
Tanmay Mare <105500424+tanmaymare@users.noreply.github.com>
2025-06-30 13:28:33 +0530

    Add files via upload

---
+++ Vivado_simulations/Half Adder/half_adder.v
@@ -0,0 +1,31 @@
+//`timescale 1ns / 1ps
+////////////////////////////////////////////////////////////////////////////////////
+//// Company: 
+//// Engineer: 
+//// 
+//// Create Date: 06/24/2025 03:26:24 PM
+//// Design Name: 
+//// Module Name: half_adder
+//// Project Name: 
+//// Target Devices: 
+//// Tool Versions: 
+//// Description: 
+//// 
+//// Dependencies: 
+//// 
+//// Revision:
+//// Revision 0.01 - File Created
+//// Additional Comments:
+//// 
+////////////////////////////////////////////////////////////////////////////////////
+
+
+//module half_adder(
+//    input A,
+//    input B,
+//    output Sum,
+//    output Carry
+//);
+//    assign Sum = A ^ B;
+//    assign Carry = A & B;
+//endmodule
